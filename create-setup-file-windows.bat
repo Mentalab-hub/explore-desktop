c13ada8a1c1edf8523658cc7cadd3a37b63a8007
bmeatayi <bmeatayi@gmail.com>
2022-01-20 16:04:31 +0100

    install openssl 1.1.1l

--- create-setup-file-windows.bat
+++ create-setup-file-windows.bat
@@ -9,8 +9,9 @@
 
 @REM # Conda virtual env
 call conda config --append channels conda-forge
-call conda create -n gui_installer python=3.8.10 -y openssl=1.1.1l
+call conda create -n gui_installer python=3.8.10 -y
 call conda activate gui_installer
+call conda install openssl=1.1.1l
 @REM call which python
 call python -m pip install --upgrade pip
 
